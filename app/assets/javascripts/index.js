4e1aa40836270d8afa410c7dfdac09d9c4760773
Amy Chan <chamy09@gmail.com>
2017-05-14 12:35:17 -0500

    added image uploader

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,7 @@
 var userimg = document.createElement('img');
 var dropbox = document.getElementById('dropbox');
+var imagebox = document.getElementById("imagebox");
+var imagetarget = document.getElementById("image");
 
 dropbox.addEventListener('dragover', function(imageFile) {
   imageFile.stopPropagation();
@@ -8,6 +10,8 @@ dropbox.addEventListener('dragover', function(imageFile) {
 });
 
 dropbox.addEventListener('drop', function(imageFile) {
+  imagetarget.src = "";
+
   imageFile.stopPropagation();
   imageFile.preventDefault();
   var userfile = imageFile.dataTransfer.files[0];
@@ -15,63 +19,67 @@ dropbox.addEventListener('drop', function(imageFile) {
     var reader = new FileReader();
     reader.onload = function(image) {
       userimg.setAttribute('src', image.target.result);
-    }
+    };
     reader.readAsDataURL(userfile);
-  }
+  };
 });
+
+document.getElementById("imguploader").onchange = function () {
+  imagebox.innerHTML = "";
+
+  var reader = new FileReader();
+
+  reader.onload = function (event) {
+    imagetarget.src = event.target.result;
+  };
+
+  reader.readAsDataURL(this.files[0]);
+};
 
 userimg.addEventListener('load', function() {
   var vibrant = new Vibrant(userimg);
   var swatches = vibrant.swatches();
 
-  document.getElementById("imagebox").innerHTML = '<img width="50%" src="' + userimg.src +'" />';
+  imagebox.innerHTML = '<img width="50%" src="' + userimg.src +'" />';
 
   var colorArray = [];
 
   if (swatches.Vibrant){
     colorVibrant = swatches.Vibrant.getHex();
-    document.getElementById("swatch1").style["background-color"] = colorVibrant;
-    document.getElementById("swatch1").style["color"] = swatches.Vibrant.getTitleTextColor();
+    document.getElementById("swatch0").style["background-color"] = colorVibrant;
     colorArray.push(colorVibrant);
   };
 
   if (swatches.Muted){
     colorMuted = swatches.Muted.getHex(); 
-    document.getElementById("swatch2").style["background-color"] = colorMuted;
-    document.getElementById("swatch2").style["color"] = swatches.Muted.getTitleTextColor();
+    document.getElementById("swatch1").style["background-color"] = colorMuted;
     colorArray.push(colorMuted);
   };
 
   if (swatches.DarkVibrant){
     colorDarkVibrant = swatches.DarkVibrant.getHex();
-    document.getElementById("swatch3").style["background-color"] = colorDarkVibrant;
-    document.getElementById("swatch3").style["color"] = swatches.DarkVibrant.getTitleTextColor();
+    document.getElementById("swatch2").style["background-color"] = colorDarkVibrant;
     colorArray.push(colorDarkVibrant);
-  }
+  };
 
   if (swatches.DarkMuted){
     colorDarkMuted = swatches.DarkMuted.getHex();
-    document.getElementById("swatch4").style["background-color"] = colorDarkMuted;
-    document.getElementById("swatch4").style["color"] = swatches.DarkMuted.getTitleTextColor();
+    document.getElementById("swatch3").style["background-color"] = colorDarkMuted;
     colorArray.push(colorDarkMuted);
-  }
+  };
 
   if (swatches.LightVibrant){
     colorLightVibrant = swatches.LightVibrant.getHex();
-    document.getElementById("swatch5").style["background-color"] = colorLightVibrant;
-    document.getElementById("swatch5").style["color"] = swatches.LightVibrant.getTitleTextColor();
+    document.getElementById("swatch4").style["background-color"] = colorLightVibrant;
     colorArray.push(colorLightVibrant);
-  }
-
-  if (swatches.LightMuted){
-    colorLightMuted = swatches.LightMuted.getHex();
-    document.getElementById("swatch6").style["background-color"] = colorLightMuted;
-    document.getElementById("swatch6").style["color"] = swatches.LightMuted.getTitleTextColor();
-    colorArray.push(colorLightMuted);
   };
 
+  // if (swatches.LightMuted){
+  //   colorLightMuted = swatches.LightMuted.getHex();
+  //   document.getElementById("swatch5").style["background-color"] = colorLightMuted;
+  //   colorArray.push(colorLightMuted);
+  // };
+
   document.getElementById("color_array").value = colorArray;
+  // console.log(colorArray);
 });
-
-
-// http post colors
--- app/controllers/palettes_controller.rb
+++ app/controllers/palettes_controller.rb
@@ -5,10 +5,13 @@ class PalettesController < ApplicationController
 
   def create
     @user_id = 1
+    #change id after users set up
 
     @palette = Palette.new(user_id: @user_id)
 
-    @color_array = params[:color_array]
+    color_array = params[:color_array]
+
+    @color_array = color_array.split(",")
 
     @color_array.each do |color|
       @palette_color = Color.find_or_create_by(value: color)
